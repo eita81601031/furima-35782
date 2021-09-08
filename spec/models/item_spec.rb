f8a52d23af0cd29e01a7b11c568824dcf17f6d1e
eita81601031 <85558700+eita81601031@users.noreply.github.com>
2021-09-08 17:27:25 +0900

    改行削除

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,14 +12,12 @@ RSpec.describe Item, type: :model do
         expect(@item).to be_valid
       end
     end
-
     context '商品出品登録できないとき' do
       it '画像が空では登録できない' do
         @item.image = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
-    
       it '商品名が空では登録できない' do
         @item.product_name = ''
         @item.valid?
