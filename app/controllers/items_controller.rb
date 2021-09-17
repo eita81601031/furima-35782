8d108ac702b89f2fea40989d138ce6ffbbda9f0f
eita81601031 <85558700+eita81601031@users.noreply.github.com>
2021-09-17 19:03:27 +0900

    コメントアウト削除、before_action追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-
-  def move_to_index
-    redirect_to new_user_session_path unless user_signed_in?
-  end
+  before_action :set_item , only: [:edit, :show, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -23,16 +20,13 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def update
-    @item = Item.find(params[:id])
     @item.update(item_params)
     if @item.save
       redirect_to item_path
@@ -47,4 +41,7 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:product_name, :description, :price, :status_id, :shipping_cost_id, :prefecture_id, :shipping_id, :category_id, :image).merge(user_id: current_user.id)
   end
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
